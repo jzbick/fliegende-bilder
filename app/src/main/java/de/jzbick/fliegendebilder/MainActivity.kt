47a0e03ddd9677585de3a98b055794c953bbfa64
Jonathan Zbick <jonathan.zbick@newcubator.com>
2021-04-09 14:58:54 +0200

    add real gps coordinates

--- app/src/main/java/de/jzbick/fliegendebilder/MainActivity.kt
+++ app/src/main/java/de/jzbick/fliegendebilder/MainActivity.kt
@@ -1,6 +1,10 @@
 package de.jzbick.fliegendebilder
 
+import android.content.Context
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.sceneform.Node
 import com.google.ar.sceneform.rendering.ViewRenderable
@@ -10,13 +14,32 @@ import uk.co.appoly.arcorelocation.LocationMarker
 import uk.co.appoly.arcorelocation.LocationScene
 import uk.co.appoly.arcorelocation.utils.ARLocationPermissionHelper
 import java.util.concurrent.CompletableFuture
-import de.jzbick.fliegendebilder.Location
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var arFragment: ArFragment
     private var locationScene: LocationScene? = null
-    private var locations: ArrayList<Location> = ArrayList<Location>()
+    private lateinit var location: android.location.Location
+    private var locations: ArrayList<Location> = ArrayList()
+    private lateinit var locationManager: LocationManager
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(loc: android.location.Location?) {
+            location = loc!!
+        }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+            Log.e("LOCATION_ERROR", "$p0, $p1, $p2")
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+            Log.e("LOCATION_ERROR", "$p0")
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+            Log.e("LOCATION_ERROR", "$p0")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,15 +51,24 @@ class MainActivity : AppCompatActivity() {
             ARLocationPermissionHelper.requestPermission(this)
         }
 
-        this.loadLocations()
-        this.init()
+        if (ARLocationPermissionHelper.hasPermission(this)) {
+
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5f, locationListener)
+
+            this.loadLocations()
+            this.init()
+        }
     }
 
     private fun loadLocations() {
         locations.add(Location(51.505454, 7.472779))
-        locations.add(Location(51.5047335,7.4704443))
-        locations.add(Location(51.5047376,7.4674283))
-        locations.add(Location(51.5023117,7.4639605))
+        locations.add(Location(51.5047335, 7.4704443))
+        locations.add(Location(51.5047376, 7.4674283))
+        locations.add(Location(51.5023117, 7.4639605))
     }
 
     override fun onResume() {
@@ -55,9 +87,7 @@ class MainActivity : AppCompatActivity() {
                     for (location in locations) {
                         val locationMarker = location.toLocationMarker(viewRenderable)
 
-                        val myLatitude: Double = 51.5059775
-                        val myLongitude: Double = 7.4736377
-                        val distance = location.getDistanceTo(myLatitude, myLongitude)
+                        val distance = location.getDistanceTo(this.location.latitude, this.location.longitude)
 
                         if (distance <= 1) {
                             locationMarker.scaleModifier = (1 - distance + 0.1).toFloat();
@@ -74,14 +104,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun createViewRenderable(): CompletableFuture<ViewRenderable>? {
         return ViewRenderable.builder()
-                .setView(this, R.layout.test_image)
-                .build()
+            .setView(this, R.layout.test_image)
+            .build()
     }
 
     private fun renderableToLocationMarker(
-            renderable: ViewRenderable,
-            longitude: Double,
-            latitude: Double
+        renderable: ViewRenderable,
+        longitude: Double,
+        latitude: Double
     ): LocationMarker {
         val node = Node()
 
