7cb59a97cd007fb786d3289f88ce013c82e5a4b4
Jonathan Zbick <jonathan.zbick@newcubator.com>
2021-04-16 10:32:22 +0200

    remove interfaces

--- app/src/main/java/de/jzbick/fliegendebilder/interfaces/SightCallback.kt
+++
@@ -1,7 +0,0 @@
-package de.jzbick.fliegendebilder.interfaces
-
-import de.jzbick.fliegendebilder.Sight
-
-fun interface SightCallback {
-    fun onSuccess(sightArray: ArrayList<Sight>)
-}
--- app/src/main/java/de/jzbick/fliegendebilder/interfaces/SightImageCallback.kt
+++
@@ -1,7 +0,0 @@
-package de.jzbick.fliegendebilder.interfaces
-
-import de.jzbick.fliegendebilder.SightImage
-
-fun interface SightImageCallback {
-    fun onSuccess(imageArray: ArrayList<SightImage>)
-}
